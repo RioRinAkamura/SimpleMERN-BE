03425f30f6db980dd1b4684eac3effa78cecffae
RioRinAkamura <nguyenthanhrin26@gmail.com>
2022-05-12 17:34:56 +0700

    fix

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv")
-var bodyParser = require("body-parser")
-const mongoose = require("mongoose")
-const authRouter = require('./routes/auth')
-const postRouter = require('./routes/post')
-const app = express()
+const dotenv = require("dotenv");
+var bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
+const app = express();
 
-dotenv.config()
+dotenv.config();
 // CONNECT DATABASE
 // mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.eivch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, () => {
 
@@ -19,20 +19,18 @@ const connectDB = async () => {
         await mongoose.connect(
             `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.eivch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
             {
-                useCreateIndex: true,
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
-                useFindAndModify: false
             }
-        )
+        );
 
-        console.log('MongoDB connected')
+        console.log("MongoDB connected");
     } catch (error) {
-        console.log(error.message)
-        process.exit(1)
+        console.log(error.message);
+        process.exit(1);
     }
-}
-connectDB()
+};
+connectDB();
 
 // app.use(bodyParser.json({ limit: '50bm' }));
 app.use(express.json());
@@ -42,6 +40,6 @@ app.use(cors());
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
