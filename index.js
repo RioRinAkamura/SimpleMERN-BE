0987470068c3ce6751712b50c364f5a4be2c7865
RioRinAkamura <nguyenthanhrin26@gmail.com>
2022-05-12 19:49:46 +0700

    fix7

--- index.js
+++ index.js
@@ -10,17 +10,34 @@ const app = express();
 dotenv.config();
 
 // CONNECT DATABASE
-mongoose.connect(
-    // process.env.DATABASE_URL,
-    "mongodb+srv://rinnguyen:nguyenthanhrin260396@cluster0.eivch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-        useNewUrlParser: "true",
-        useUnifiedTopology: "true",
-    },
-    () => {
-        console.log("Connected to MongoDB");
+// mongoose.connect(
+//     // process.env.DATABASE_URL,
+//     "mongodb+srv://rinnguyen:nguyenthanhrin260396@cluster0.eivch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+//     {
+//         useNewUrlParser: "true",
+//     },
+//     () => {
+//         console.log("Connected to MongoDB");
+//     }
+// );
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            "mongodb+srv://rinnguyen:nguyenthanhrin260396@cluster0.eivch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+            {
+                useNewUrlParser: true,
+            }
+        )
+
+        console.log('MongoDB connected')
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
     }
-);
+}
+
+connectDB()
 
 // app.use(bodyParser.json({ limit: '30bm' }));
 app.use(express.json());
