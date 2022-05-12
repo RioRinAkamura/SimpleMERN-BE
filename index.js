61e6bca47e07c0f8e2d0b4fa7bab26468404fc9f
RioRinAkamura <nguyenthanhrin26@gmail.com>
2022-05-12 17:25:50 +0700

    change index.js

--- index.js
+++ index.js
@@ -9,9 +9,30 @@ const app = express()
 
 dotenv.config()
 // CONNECT DATABASE
-mongoose.connect(process.env.DATABASE_URL, () => {
-    console.log("Connected to MongoDB");
-})
+// mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.eivch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, () => {
+
+// console.log("Connected to MongoDB");
+// })
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.eivch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+            {
+                useCreateIndex: true,
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useFindAndModify: false
+            }
+        )
+
+        console.log('MongoDB connected')
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
+    }
+}
+connectDB()
 
 // app.use(bodyParser.json({ limit: '50bm' }));
 app.use(express.json());
